97a1d120d351795c5f10e848d4e3b04c08414f46
Nana APPIAH O. G <aonana@ecobank.group>
2022-03-15 11:50:02 +0000

    Drag

---
+++ PETAS/Services/IDragService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Diagnostics;
+
+namespace PETAS.Services
+{
+    public interface IDragService
+    {
+        public Task<bool> AssignTrainingAsync();
+    }
+
+    public class DragService: IDragService
+    {
+        private readonly HttpClient http;
+        public DragService(HttpClient _httpclient)
+        {
+            http = _httpclient;
+        }
+
+        public async Task<bool> AssignTrainingAsync()
+        {
+            return true;
+        }
+    }
+
+}
