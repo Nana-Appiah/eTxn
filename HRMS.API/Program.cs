0f9c2383994d9e09ca03b1f7d5d92ba08d6ae707
Nana APPIAH O. G <aonana@ecobank.group>
2022-03-29 20:31:20 +0000

    Dashboard

--- HRMS.API/Program.cs
+++ HRMS.API/Program.cs
@@ -21,14 +21,20 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
-options.AddPolicy(Cors,
-            builder => builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .SetPreflightMaxAge(TimeSpan.FromSeconds(5000))
-            .SetIsOriginAllowed(x => true)
-            .WithExposedHeaders("*")
-    );
+        options.AddPolicy(Cors,
+                //builder => builder.AllowAnyOrigin()
+                //.AllowAnyMethod()
+                //.AllowAnyHeader()
+                //.SetPreflightMaxAge(TimeSpan.FromSeconds(5000))
+                //.SetIsOriginAllowed(x => true)
+                //.WithExposedHeaders("*")
+
+                builder => builder.AllowAnyOrigin()
+                   .WithMethods("POST", "GET", "HEAD", "OPTIONS", "DELETE")
+                     .WithHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Content-Disposition", "Content-Type", "Authorization", "Accept", "Origin", "Host", "api-user") //
+                     .SetPreflightMaxAge(TimeSpan.FromSeconds(5000))
+                     .SetIsOriginAllowed(x => true)
+            );
 });
 
 
